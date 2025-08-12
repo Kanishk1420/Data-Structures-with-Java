7f8a637a1657821d6f0c12f5423dc45685955457
Kanishk <150684142+Kanishk1420@users.noreply.github.com>
2025-08-12 20:26:59 +0530

    Welcome Back Imporved Code of Prefix sub array sum

--- Self/Chap7 Arrays/Maxsubarray_Prefixsum.java
+++ Self/Chap7 Arrays/Maxsubarray_Prefixsum.java
@@ -1,32 +1,30 @@
 public class Maxsubarray_Prefixsum {
-    public static void prefix(int arr[]){
-        int maxSum = Integer.MIN_VALUE;
-        int prefix[] = new int[arr.length];
-        
-        // Calculate prefix array
-        prefix[0] = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            prefix[i] = prefix[i-1] + arr[i];
-            System.out.println(prefix[i]);
-        }
-
-        // Calculate max subarray sum using prefix array
-        for (int i = 0; i < arr.length; i++) {
-            int currSum = 0;
-            for (int j = i; j < arr.length; j++) {
-                // Sum of subarray arr[i...j]
-                currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i-1];
-                
-                // Update maximum sum found so far
-                maxSum = Math.max(maxSum, currSum);
-            }
-        }
-        System.out.println("Maximum subarray sum is: " + maxSum); 
+public static int prefix(int arr[]){
+int prefix[] = new int[arr.length];
+int maxSum = Integer.MIN_VALUE;
+int currentsum = 0;
+prefix[0] = arr[0];
+for(int i=1; i<arr.length; i++){
+ prefix[i] = prefix[i-1]+arr[i];
+}
+for(int i = 0; i<arr.length; i++){
+  currentsum = 0;
+  for(int j = i; j<arr.length; j++){
+  if(i == 0){
+    currentsum = prefix[j];
+  }
+    else{
+      currentsum = prefix[j] - prefix[i-1];
+    }
+    maxSum = Math.max(currentsum, maxSum);
+  }
+}
+return maxSum;
     }
     
     public static void main(String[] args) {
         int arr[] = {1,-2,6,-1,3};
-        prefix(arr);
+        System.out.println("Maximum Subarray sum by using prefix sum method is "+prefix(arr));
     }
 }
 // Time complexity is 0(n2);
