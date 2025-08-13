2068ad6bda08927080c41fa32c417b2a926ebe45
Kanishk <150684142+Kanishk1420@users.noreply.github.com>
2025-08-14 02:14:59 +0530

    added comment

--- Self/Chap7 Arrays/Trapped_Rainwater.java
+++ Self/Chap7 Arrays/Trapped_Rainwater.java
@@ -7,13 +7,13 @@ public class Trapped_Rainwater {
         for (int i = 0; i < left.length; i++) {
             first = Math.max(first, height[i]);
             System.out.print(first);
-            left[i] = first;
+            left[i] = first; // hamne yaha isse kyo raha ? because left[i] mei jab ham agge loop sei min dudge tab agge jake first kei final value 6 hogi and similar to right to thats why 
         }
         System.err.println(" ");
         for (int i = right.length - 1; i >= 0; i--) {
             last = Math.max(last, height[i]);
             System.out.print(last);
-            right[i] = last;
+            right[i] = last;  // hamne yaha isse kyo raha ? because right[i] mei jab ham agge loop sei min dudge tab agge jake last kei final value 6 hogi and similar to right to thats why 
         }
         int trappedWater = 0;
         System.err.println(" ");
