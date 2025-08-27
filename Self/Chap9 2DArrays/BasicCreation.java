eead0b39b8986fb6996797218fbcf79cbb69c698
Kanishk <150684142+Kanishk1420@users.noreply.github.com>
2025-08-27 17:19:28 +0530

    Seprate Creation of matrix

---
+++ Self/Chap9 2DArrays/BasicCreation.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BasicCreation {
+    public static void main(String args[]){
+        // creation
+    int matrix[][] = new int[3][3];
+    Scanner sc = new Scanner(System.in);
+    // input
+    for (int i = 0; i < matrix.length; i++) {
+    for (int j = 0; j < matrix[0].length; j++) {
+     matrix[i][j] = sc.nextInt();   
+    }
+}
+// output
+for (int i = 0; i < matrix.length; i++) {
+    for (int j = 0; j < matrix[0].length; j++) {
+        System.out.print(matrix[i][j]+" ");
+    }    
+    System.err.println();
+}
+sc.close();
+}
+}
---
+++ Self/Chap9 2DArrays/Linearsearch.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+// searching element 
+public class Linearsearch {
+   public static boolean search(int arr[][],int key){
+    for (int i = 0; i < arr.length; i++) {
+    for (int j = 0; j < arr[0].length; j++) {
+        if (arr[i][j]==key) {
+            System.err.print("Found at cell ("+i+","+j+")");
+            return true;
+        }
+    }
+    System.err.println("");
+   }
+   System.out.println("Key not found");
+   return false;
+   }
+   public static int smallest(int arr[][]){
+   int smallest = Integer.MAX_VALUE;
+   for (int i = 0; i < arr.length; i++) {
+    for (int j = 0; j < arr[0].length; j++) {
+        smallest =  Math.min(smallest, arr[i][j]);
+    }
+   }
+   return smallest;
+   }
+   public static int largest(int arr[][]){
+    int largest = Integer.MIN_VALUE;
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr[0].length; j++) {
+        largest = Math.max(largest, arr[i][j]);
+      }
+    }
+    return largest;
+   }
+   public static void main(String[] args) {
+   int arr[][] = new int[3][3];
+   Scanner sc = new Scanner(System.in);
+    System.err.println("Enter the number ");
+   for (int i = 0; i < arr.length; i++) {
+    for (int j = 0; j < arr[0].length; j++) {
+      arr[i][j] = sc.nextInt();
+    }
+   }
+   System.err.println("The matrix:- ");
+    for (int i = 0; i < arr.length; i++) {
+    for (int j = 0; j < arr[0].length; j++) {
+      System.err.print(arr[i][j]+" ");
+    }
+    System.err.println("");
+   }
+   sc.close();
+   search(arr, 3);
+   System.err.println("\n");
+   System.err.println("Smallest element in the array is "+smallest(arr));
+   System.out.println("Largest element in the array is "+largest(arr));  
+   }
+}
+
