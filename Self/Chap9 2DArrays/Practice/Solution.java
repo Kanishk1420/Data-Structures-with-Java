88a1cf76827dea4f3c471a59ad3934dcec750112
Kanishk <150684142+Kanishk1420@users.noreply.github.com>
2025-08-31 12:02:12 +0530

    Added New question leetcode 2525

--- Self/Chap9 2DArrays/Practice/Solution.java
+++ Self/Chap9 2DArrays/Practice/Solution.java
@@ -1,15 +1,23 @@
-import java.util.*;
 class Solution {
-    public static boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for (int i = 1; i < nums.length; i++) {
-            if(nums[i]==nums[i-1])
-            return true;
-        }
-        return false;
+    public static String categorizeBox(int length, int width, int height, int mass) {
+        long volume = (long)length*width*height;
+        boolean isBulky = false;
+        boolean isHeavy = false;
+        
+        if (length>=Math.pow(10,4) || width>=Math.pow(10,4) || height>=Math.pow(10,4) || volume>= Math.pow(10, 9))
+            isBulky = true;
+        if(mass >= 100)
+            isHeavy = true;
+            
+        if (isBulky && isHeavy)
+            return "Both";
+        if(!isBulky && !isHeavy)
+            return "Neither";
+        if(isBulky && !isHeavy)
+            return "Bulky";
+        return "Heavy";
     }
     public static void main(String[] args){
-        int nums[] = {1,2,3,4,6};
-        System.out.println(containsDuplicate(nums));
+        System.out.println(categorizeBox(2909, 3968,3272,727));  // Now outputs "Both"
     }
 }
